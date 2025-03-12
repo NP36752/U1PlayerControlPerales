a2b99262242dc285d79a6a06e1802e48ddb911f1
NP36752 <735430@student.dusd.net>
2025-03-12 09:34:20 -0700

    fixed camera positioning
    i misread the instructions and put the camera behind the plane instead of beside

--- U1PlayerControlPerales/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ U1PlayerControlPerales/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset = new Vector3(0, 3, -15);
+    private Vector3 offset = new Vector3(15, 3, 0);
 
     // Start is called before the first frame update
     void Start()
